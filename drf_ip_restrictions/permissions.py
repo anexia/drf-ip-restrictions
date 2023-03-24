1138306e7ec965efe28b6c9c718f1f9264b9a12e
Armin Ster <sterarmin@gmail.com>
2023-03-24 20:35:01 +0100

    Make ip list setting changeable

--- drf_ip_restrictions/permissions.py
+++ drf_ip_restrictions/permissions.py
@@ -7,12 +7,14 @@ class AllowedIpList(object):
     Ensure the request's IP address is on the ip white list configured in Django settings.
     """
 
+    setting_ip_list = "ALLOWED_IP_LIST"
+
     def has_permission(self, request, view):
         client_ip, is_routable = get_client_ip(request)
 
         if client_ip:
             settings = ip_restriction_settings()
-            allowed_ips = settings.ALLOWED_IP_LIST
+            allowed_ips = getattr(settings, self.setting_ip_list)
             for allowed_ip in allowed_ips:
                 if client_ip == allowed_ip or client_ip.startswith(allowed_ip):
                     return True
